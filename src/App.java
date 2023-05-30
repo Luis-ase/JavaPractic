b7dccab4bc8b5405292150d378c41286f9f2a94a
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-05-30 18:55:01 -0300

    first commit

--- src/App.java
+++ src/App.java
@@ -13,11 +13,19 @@ public class App {
         float versionCsgo= 1.6f;
         String letra = "a";
         String name= "Luis";
+        int phonenumber, adreass, numberPiso;
+        phonenumber = 1199999999;
+        adreass = 8888;
+        numberPiso = 777777;
+        final String nombreCompleto = "Michael Luis";
 
         System.out.println(contador);
         System.out.println(name);
         System.out.println(login);
         System.out.println(versionCsgo);
+        System.out.println(phonenumber);
+        System.out.println(adreass);
+        System.out.println(numberPiso);
 
     }
 }

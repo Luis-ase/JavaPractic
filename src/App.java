48742f940bfbb9425f4075647ae707eaf27776ab
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-05-31 22:05:53 -0300

    Practic Java 02

--- src/App.java
+++ src/App.java
@@ -9,27 +9,6 @@ public class App {
          * 
          */
         int contador = 100;
-        for (int i = 1; i < contador; i++) {
-            String letras = "Fizz", bus = "Buzz";
-            String Completo;
-            if(i%3 ==0 && i%5 ==0 ){
-                Completo = letras +bus;
-                System.out.print(Completo);
-                System.out.println(i);
-                
-            }
-            else if(i%3==0){
-                Completo = letras;
-                System.out.print(Completo);
-                System.out.println(i);
-            }
-            else if(i%5==0){
-                Completo = bus;
-                System.out.print(Completo);
-                System.out.println(i);
-            }
-
-            
-        }
+        
     }
 }

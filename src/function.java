4d7eed683b9f73c4f4752815406a058a00e8f701
Michael Luis Condori Paye <luis2003nb@gmail.com>
2023-06-02 16:03:00 -0300

    Practi java 03

--- src/function.java
+++ src/function.java
@@ -1,17 +1,37 @@
 public class function {
     public static void main(String[] args) {
-        var numero = multiplicar(20, 10);
-        System.out.println(numero);
-        hi("luis", 19);
+        // var numero = multiplicar(20, 10);
+        // System.out.println(numero);
+        // hi("luis", 19);
+        Recursividad01(10);
+        System.out.println(Recursividad02(4));
 
     }
     // para que esta funcion sea invocado  se necesita invocar
     // adentro del metodo main
-    static void hi(String nombre, int edad){
-         System.out.printf("hola %s tu edad es %d \n",nombre, edad);
+    // static void hi(String nombre, int edad){
+    //      System.out.printf("hola %s tu edad es %d \n",nombre, edad);
+    // }
+    // static int multiplicar(int a, int b){
+    //     int suma = a * b;
+    //     return suma;
+    // }
+    
+
+    //Recursividad
+    static int Recursividad01(int numero){
+        numero--;
+        if(numero >0){
+            System.out.println(numero);
+            return Recursividad01(numero);
+        }
+        return numero;
     }
-    static int multiplicar(int a, int b){
-        int suma = a * b;
-        return suma;
+    static int Recursividad02(int numero){
+        if(numero >1){
+            numero = numero * Recursividad02(numero -1);
+        }
+        return numero;
     }
+
 }
